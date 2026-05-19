43bdab2bfedcd42e5516539a3b4713c1d9b057f6
Fredrik Forsell <fredrik.forsell@golfnow.com>
2026-05-19 17:14:38 -0400

    [expo-video][plugin] Add IMA SDK config plugin support
    Adds Expo config plugin support for enabling Google IMA SDK:
    - Configures Android Gradle properties for IMA dependency inclusion
    - Sets iOS pod property to enable GoogleInteractiveMediaAds pod

--- packages/expo-video/plugin/build/withExpoVideo.d.ts
+++ packages/expo-video/plugin/build/withExpoVideo.d.ts
@@ -2,7 +2,11 @@ import { type ConfigPlugin } from 'expo/config-plugins';
 export type WithExpoVideoOptions = {
     /** Whether to enable background playback support. */
     supportsBackgroundPlayback?: boolean;
+<<<<<<< HEAD
+    supportsInteractiveMediaAds?: boolean;
+=======
     /** Whether to enable Picture-in-Picture on Android and iOS. */
+>>>>>>> main
     supportsPictureInPicture?: boolean;
 };
 declare const withExpoVideo: ConfigPlugin<WithExpoVideoOptions>;
